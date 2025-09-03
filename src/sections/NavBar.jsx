2af323532e3b6269c0dc3c6050b0b371739bf0e8
M Abrar Khalid <iabrarkhalidofficial@gmail.com>
2025-09-03 15:53:38 +0500

    Update NavBar.jsx

--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -1,10 +1,14 @@
 const NavBar = () => {
-  return (
-    <nav>
-        <img src="/images/nav-logo.svg" className="scale-90" />
-        <img src="/images/menu.svg" className="w-10" />
-      </nav>
-  )
-}
+  const handleNavClick = () => {
+    window.open("https://github.com/your-github-abrarkhalidofficial", "_blank");
+  };
 
-export default NavBar
+  return (
+    <nav onClick={handleNavClick} style={{ cursor: "pointer" }}>
+      <img src="/images/nav-logo.svg" className="scale-90" />
+      <img src="/images/menu.svg" className="w-10" />
+    </nav>
+  );
+};
+
+export default NavBar;
